42a9e1ed31238ff72f884f6de0a2989c9a0eac96
lxymahatma <326265648@qq.com>
2024-02-01 21:38:54 +0800

    bump version

--- MelonBuildInfo.cs
+++ MelonBuildInfo.cs
@@ -8,5 +8,5 @@ internal static class MelonBuildInfo
 
     internal const string Author = "Creepler13 & lxy";
 
-    internal const string ModVersion = "2.0.0";
+    internal const string ModVersion = "2.0.1";
 }
